1502fcc1fac1624734da7c3cde6450c966353632
Intengine <studio@intengine.pl>
2019-03-15 19:45:38 +0100

    Added golem movement
    Added simple script, froze golem's rotation.

---
+++ Rescue Golem/Assets/Scripts/GolemMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemMovement : MonoBehaviour
+{
+    private Rigidbody golemBody;
+    private float moveForce = 10f;
+
+    void Awake()
+    {
+        golemBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        golemBody.velocity = new Vector3(-h * moveForce, 0f, 0f);
+    }
+}
