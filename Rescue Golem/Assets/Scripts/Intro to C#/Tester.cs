d801c195aa0af6fb899a455198787661abd9d2e1
Intengine <studio@intengine.pl>
2019-03-19 18:08:53 +0100

    Added Tester class for learning
    For my coleague.

---
+++ Rescue Golem/Assets/Scripts/Intro to C#/Tester.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    void Start()
+    {
+        CalculateTwoNumbers(4, 8);
+        print("The second calculation is " + SecondCalculation());
+        print("The third calculation is " + ThirdCalculation(88, 223));
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void CalculateTwoNumbers(int a, int b)
+    {
+        print("The sum of two numbers is " + (a + b));
+    }
+
+    int SecondCalculation()
+    {
+        return 5 + 6;
+    }
+
+    int ThirdCalculation(int a, int b)
+    {
+        return a + b;
+    }
+}
