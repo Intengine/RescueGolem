7c5f0d44e1fe52ea9cc69d861cf6595233fdf07c
Intengine <studio@intengine.pl>
2019-03-26 17:54:35 +0100

    Added delegates
    Still learning.

---
+++ Rescue Golem/Assets/Scripts/Intro to C#/Delegates.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// delegate is like a letter that we need to deliver to somebody but we don't care who wants to deliver that letter
+// event from that deliver is the letter and we want somebody to deliver that letter to point "X"
+
+public class Delegates : MonoBehaviour
+{
+    // first function that is going to call
+    void Awake()
+    {
+
+    }
+
+    // third function that is going to call
+    void Start()
+    {
+        
+    }
+
+    // second function that is going to call
+    void OnEnable()
+    {
+        Wizard.playerDied += ExecutedAfterEventCall; // it is subscribing to the PlayerDied event ("ok, I've got that letter, I will deliver it") -> in that case, the letter is delivering by Delegates class
+    }
+
+    // that function is called after quit the game
+    void OnDisable()
+    {
+        Wizard.playerDied -= ExecutedAfterEventCall; // it is unsubscribing to the PlayerDied event
+    }
+
+    void ExecutedAfterEventCall()
+    {
+        print("Printed after call was executed");
+    }
+}
--- Rescue Golem/Assets/Scripts/Intro to C#/Wizard.cs
+++ Rescue Golem/Assets/Scripts/Intro to C#/Wizard.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class Wizard : Player // inheriting from Player class
 {
     public static int power = 100; // a static variable, so called a "class variable"
+    public delegate void PlayerDied();
+    public static event PlayerDied playerDied; // this is our "letter"
+
+    void Start()
+    {
+        StartCoroutine(CallFunction());
+    }
+
+    IEnumerator CallFunction()
+    {
+        // "ok, does anybody have that letter?" -> simply checks if anybody have our event ("letter")
+        yield return new WaitForSeconds(2f);
+        if(playerDied != null)
+        {
+            playerDied(); // "ok, deliver it now"
+        }
+    }
 
     public Wizard()
     {
