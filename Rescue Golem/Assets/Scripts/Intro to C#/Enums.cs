4db4c3e26355612aaebd66e6f9112981fbc38987
Intengine <studio@intengine.pl>
2019-04-01 18:02:37 +0200

    Added some enums
    Still teaching.

---
+++ Rescue Golem/Assets/Scripts/Intro to C#/Enums.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums : MonoBehaviour
+{
+    public enum GameState
+    {
+        Started,
+        Paused,
+        Ended,
+        PlayerDied,
+        MainMenu
+    }
+
+    GameState gameState = GameState.MainMenu;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
