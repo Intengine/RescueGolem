fc857f6f6732c7cad33063e2a9d0ad5c7d90212b
Intengine <studio@intengine.pl>
2019-04-01 18:25:16 +0200

    Added some arrays
    Still teaching.

---
+++ Rescue Golem/Assets/Scripts/Intro to C#/Arrays.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+    public List<int> items = new List<int>(); // list (resizable)
+    public int[] backpack = new int[10]; // array (fixed size - if it's 10, then it can't be more or less than 10)
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
